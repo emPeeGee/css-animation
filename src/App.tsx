91089d17a460ffabd558f6e1d70276d4901086ca
Mihail Popovici <mihail.popovici@energymarketprice.com>
2021-05-06 18:09:43 +0300

    feat: Add vodafone animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import { TransitionOptions } from './components/ChapterOne/TransitionOptions/TransitionOptions';
-import { Transform } from './components/ChapterOne/Transform/Transform';
-import { WhatIsTransition } from './components/ChapterOne/WhatIsTransition/WhatIsTransition';
-
-import './App.scss';
 import { ChapterOne } from './components/ChapterOne/ChapterOne';
 import { Buttons } from './components/Buttons/Buttons';
+import { Vodafone } from './components/Vodafone/Vodafone';
+
+import './App.scss';
 function App() {
   return (
     <div className="App">
@@ -21,12 +19,18 @@ function App() {
               <li>
                 <Link to="/buttons">Buttons</Link>
               </li>
+              <li>
+                <Link to="/vodafone">Vodafone</Link>
+              </li>
             </ul>
           </nav>
 
           <Switch>
             <Route path="/buttons">
               <Buttons />
+            </Route>
+            <Route path="/vodafone">
+              <Vodafone />
             </Route>
             <Route path="/">
               <ChapterOne />
---
+++ src/components/Vodafone/Vodafone.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+import logo2 from '../../assets/logo2.png';
+
+import './Vodafone.scss';
+
+export function Vodafone() {
+  return (
+    <div className="wrapper">
+      <div className="Vodafone">
+        <img width="47" src={logo2} alt="" />
+      </div>
+
+      <h1>DSDS</h1>
+    </div>
+  );
+}
